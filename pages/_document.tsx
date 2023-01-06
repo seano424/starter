44d92be80626c1a43478054b1c94698b8ea7638b
Sean OReilly <soreilly424@gmail.com>
2023-01-06 09:58:31 -0500

    adding debug screens

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import clsx from 'clsx'
 
 export default function Document() {
+  const devMode = process.env.ENVIRONMENT === 'dev'
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className={clsx(devMode && 'debug-screens')}>
         <Main />
         <NextScript />
       </body>
